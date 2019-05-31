87972460680ce2c8ef92b92d6de65caa3d272c4a
suaibkhan <suaibk946@gmail.com>
2019-05-31 13:36:38 +0530

    second

---
+++ app/src/main/java/com/example/githubfirstapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.githubfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btngithub);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Github",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
